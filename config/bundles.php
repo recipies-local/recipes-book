99a0d35d92259ce2ecee80c461608549caefacb8
Arnaud Gouel <gouel.arnaud@gmail.com>
2025-04-08 17:08:37 +0200

    Add initial configuration for Doctrine and EasyAdmin, and set up CSRF protection

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,8 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
